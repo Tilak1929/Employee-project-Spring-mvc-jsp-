ef5cb3a76a18bf47aa69e394877e57826f23e8e8
Tilak1929 <tilakraj2219@gmail.com>
2025-12-15 16:20:21 +0530

    removed

--- src/main/java/in/ps/product/exception/BookNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package in.ps.product.exception;
-
-public class BookNotFoundException extends RuntimeException{
-
-	public BookNotFoundException(String message) {
-		super(message);
-		System.out.println("iniside the class");
-	}
-}
--- src/main/java/in/ps/product/repo/Ibookrepo.java
+++
@@ -1,9 +0,0 @@
-package in.ps.product.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import in.ps.product.entity.Book;
-
-public interface Ibookrepo extends JpaRepository<Book, Integer> {
-
-}
